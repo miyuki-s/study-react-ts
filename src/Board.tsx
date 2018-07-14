679e9f3a7063fc6bc4fbb2d9cfc6c2b64e47a006
Miyuki Suzuki <miyuki.s.0419.8680@gmail.com>
2018-07-15 03:06:27 +0900

    update

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,35 +1,22 @@
 import * as React from 'react';// パッケージのreactからReactという名前でインポート
 import Square from './Square';// Scuare.tsxからScuareというクラスをインポート
 
-interface InterfaceBoardState{ // InterfaceBoardStateという名前のinterface(たい焼きの具の中身の種類)を作り
-  squares: string[]; // squaresという変数の型を文字列の配列と定義
-  xIsNext: boolean; // xIsNextという変数の型をboolean(正誤判断のやつ)に定義
+interface InterfaceSquareProps{
+  squares: string[];
+  onClick: (i:number) => void;
 }
 
-export default class Board extends React.Component<{}, InterfaceBoardState> { // Boardクラス(たい焼きの型)を作成しインポートしたReactからコンポーネントクラスを継承。また、このクラス内で使うstateもしくは外部から受け取ったpropsを定義。 Boardが呼び出された時はデフォルトでこのクラスをエクスポートするようにする。
+export default class Board extends React.Component<InterfaceSquareProps, {}> { // Boardクラス(たい焼きの型)を作成しインポートしたReactからコンポーネントクラスを継承。また、このクラス内で使うstateもしくは外部から受け取ったpropsを定義。 Boardが呼び出された時はデフォルトでこのクラスをエクスポートするようにする。
   constructor(props:any) { // コンストラクタで変数の中身を決める(propsの中の値はなんでも良い。)
     super(props); // propsの中身をReactのコンポーネントクラスから継承
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext : true,
-    }; // stateの中をsquareを長さが9で全て空の配列に、xIsNextをtrueに指定
-
     this.renderSquare = this.renderSquare.bind(this); // renderSquareにthisを結びつけてそれをrenderSquareに代入
   }
 
   public render() { // 出力する
-    const winner = this.calculateWinner(this.state.squares); // winnerという変数にthis.state.squaresを引数にとるcalculateWinnerを代入(constなので今後変化しない)
-    let status; // statusという関数を今後変化する変数で置く
-    if (winner) {
-      status = 'Winner: ' + winner; // もしwinnerならstatusに'Winner: 'winner と代入
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O'); // そうでなければ'Next player: 'XまたはO と表示。(xIsNextがtrueならX, falseならOと表示)
-    }
     // status += this.state.xIsNext ? 'X':'O';
     // const status = 'Next player: X';
     return ( // 返す
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -49,48 +36,8 @@ export default class Board extends React.Component<{}, InterfaceBoardState> { //
     );
   }
 
-  private handleClick(i:number):void { // handleClickという関数をiを数字の引数として
-    const bufferSquares = this.state.squares.slice(); // this.state.squaresの配列の中身をコピーしてbufferSquaresに代入(constなので固定)
-    // bufferSquares[i] = "X";
-    if (this.calculateWinner(this.state.squares) || this.state.squares[i]) {
-     return; // もしthis.state.squaresを引数にとるcalculateWinnerまたはthis.state.squares[i]が空でないならここで処理を終了する
-
-   }
-    bufferSquares[i] = this.state.xIsNext ? 'X' : 'O'; // bufferSquares[i]にthis.state.xIsNextがtrueならX,falseならOを代入
-    this.setState({
-      squares: bufferSquares,
-      xIsNext: !this.state.xIsNext,
-    }); // squaresにbufferSquares,xIsNextにthis.state.xIsNextじゃない方をセット
-  }
-
   private renderSquare(i:number):any { // 数字iを引数にもつrenderSquareという関数は何かしらの型のものを返す
-    return <Square value={this.state.squares[i]} onClick={this.handleClick.bind(this, i)}/>; // Squareクラスの{}this.state.squares[i]
-  }
-
-  private calculateWinner(squares: string[]):string|null {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    // for (let i = 0; i < lines.length; i++) {
-    //   const [a, b, c] = lines[i];
-    //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-    //     return squares[a];
-    //   }
-    // }
-    for (const line of lines) {
-      const [a, b, c] = line;
-      if (this.state.squares[a] && this.state.squares[a] === this.state.squares[b] && this.state.squares[a] === this.state.squares[c]) {
-        return this.state.squares[a];
-      }
-    }
-    return null;
+    return <Square value={this.props.squares[i]} onClick={this.props.onClick.bind(this, i)}/>; // Squareクラスの{}this.state.squares[i]
   }
 
 }
--- src/Game.tsx
+++ src/Game.tsx
@@ -8,6 +8,7 @@ interface InterfaceHistoryState{
 interface InterfaceGameState{
   history: InterfaceHistoryState[];
   xIsNext: boolean;
+  stepNumber: number;
 }
 
 export default class Game extends React.Component<{},InterfaceGameState> {
@@ -19,21 +20,88 @@ export default class Game extends React.Component<{},InterfaceGameState> {
           squares: Array(9).fill(null),
         }
       ],
+      stepNumber: 0,
       xIsNext : true,
     }
+    this.handleClick = this.handleClick.bind(this);
   }
 
   public render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner  = this.calculateWinner(current.squares);
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li  key={move}>
+          <button onClick={this.jumpTo.bind(this,move)}>{desc}</button>
+        </li>
+      );
+    });
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} onClick={this.handleClick} />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
+  }
+
+  private jumpTo(step:number):void {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+  private handleClick(i:number):void {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([
+        {
+          squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  private calculateWinner(squares: string[]):string|null {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (const line of lines) {
+      const [a, b, c] = line;
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 }

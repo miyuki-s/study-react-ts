81c0c5b770f475984c52d36483c0c77486d6268b
Miyuki Suzuki <miyuki.s.0419.8680@gmail.com>
2018-07-14 16:11:34 +0900

    first commit

---
+++ src/Square.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'; // パッケージのreactからReactという名前でインポート
+
+interface InterfaceSquareProps { // InterfaceSquarePropsという名前のinterface(たい焼きの具の中身の種類)を作り
+  value: string; // valueの型をstring(文字)に
+  onClick: () => void; // クリックすると何も受け取らないし何も返さない
+}
+
+// interface InterfaceSquareState {
+//   value: string;
+// }
+
+// export default class Square extends React.Component<InterfaceSquareProps, InterfaceSquareState> {
+// export default class Square extends React.Component<{}, InterfaceSquareState> {
+export default class Square extends React.Component<InterfaceSquareProps, {}> { // Squareクラス(たい焼きの型)を作成しインポートしたReactからコンポーネントクラスを継承。また、このクラス内で使うstateもしくは外部から受け取ったpropsを定義。 Squareが呼び出された時はデフォルトでこのクラスをエクスポートするようにする。
+  // constructor(props: any) {
+  //   super(props);
+  //   this.handleClick = this.handleClick.bind(this)
+  //   // this.state = {
+  //   //   value: "wa",
+  //   // }
+  // }
+
+
+
+  public render() { // 出力する
+    return ( // 返す
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
+      </button> //　ボタンを設置。クリックするとthis.props.onClickを呼び出す。ボタンにはthis.props.valueを表示
+    );
+  }
+
+
+  // private handleClick():void {
+  //   this.setState({value: "X"})
+  //   // alert(this.props.value);
+  // }
+}
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Game from './Game';
+import './index.css';
+// import registerServiceWorker from './registerServiceWorker';
+//
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root') as HTMLElement
+// );
+// registerServiceWorker();
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
